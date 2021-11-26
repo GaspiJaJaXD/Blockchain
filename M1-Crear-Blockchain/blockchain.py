01b9ac5dc39e16b054bf40f82d97c4032b555adf
GaspiJaJaXD <gasparmenesesdiaz@gmail.com>
2021-11-25 22:15:16 -0300

    Update blockchain.py

--- M1-Crear-Blockchain/blockchain.py
+++ M1-Crear-Blockchain/blockchain.py
@@ -9,3 +9,17 @@ Created on Thu Nov 11 22:56:33 2021
 # Flask==0.12.2: pip install Flask==0.12.2
 # CLiente HTTP Postman: https://www.getpostman.com/ 
 
+# Importar las Librerias
+import datetime # para poder utilizar tiempo
+import hashlib
+import json 
+from flask import Flask, jsonify
+
+# Parte 1 - Crear la Cadena de BLoques
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = "0" )
+
+
+# Parte 2 - Minado de un Bloque de la Cadena
