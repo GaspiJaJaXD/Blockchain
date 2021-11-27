4610dc3856692aa266b3b876ae51d61ffaf676b1
GaspiJaJaXD <gasparmenesesdiaz@gmail.com>
2021-11-26 22:15:29 -0300

    Update blockchain.py

--- M1-Crear-Blockchain/blockchain.py
+++ M1-Crear-Blockchain/blockchain.py
@@ -20,6 +20,16 @@ class Blockchain:
     def __init__(self):
         self.chain = []
         self.create_block(proof = 1, previous_hash = "0" )
+        
+        
+    def create_block(self, proof, previous_hash):
+        block = {"index" : len(self.chain)+1,
+                 "timestamp" : str(datetime.datetime.now()),
+                 "proof" : proof, 
+                 "previous_hash" : previous_hash }
+        
+        self.chain.append(block)
+        return block
 
 
-# Parte 2 - Minado de un Bloque de la Cadena
+# Parte 2 - Minado de un Bloque de la Cadenab
