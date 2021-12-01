685a94c16850071ac799eeed552d32a6481ba56a
GaspiJaJaXD <gasparmenesesdiaz@gmail.com>
2021-11-30 23:58:18 -0300

    Update blockchain.py

--- M1-Crear-Blockchain/blockchain.py
+++ M1-Crear-Blockchain/blockchain.py
@@ -87,8 +87,12 @@ def mine_block():
                 'timestamp' : block['timestamp'],
                 'proof' : block['proof'],
                 'previous_hash' : block['previous_hash']}
-    
     return jsonify(response), 200 
 
-
+# Obtener la Blockchain
+@app.route('/get_chain', methods=['GET'])
+def get_chain():
+    response = {'chain': blockchain.chain,
+                'length': len(blockchain.chain)}
+    return jsonify(response), 200 
 
